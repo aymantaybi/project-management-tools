c3f0afc48bb2a3e7733a63dded685280bea6093b
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2022-11-17 00:16:12 +0100

    fix: refactor pert tool functions

--- pages/tools/pert.tsx
+++ pages/tools/pert.tsx
@@ -1,74 +1,99 @@
-import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot } from '@chakra-ui/react'
-import React from 'react'
+import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
+import React from "react";
 
-interface Tasks {
-    anterior: string[][],
-    current: string[],
-    subsequent: string[][],
+interface PrecedenceCondition {
+  task: string;
+  anteriors: string[];
 }
 
-const tasks: Tasks = {
-    "anterior": [
-        [],
-        [],
-        ["A"],
-        ["A", "B"],
-        ["A"],
-        ["C"],
-        ["D", "F"],
-        ["E"],
-        ["G"],
-        ["H", "I"]
-    ],
-    "current": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-    "subsequent": []
+interface TaskSubsequents {
+  task: string;
+  subsequents: string[];
+}
+
+interface Task extends PrecedenceCondition, TaskSubsequents {}
+
+interface TaskWithStep extends Task {
+  step: number | null;
+}
+
+const precedenceConditions: PrecedenceCondition[] = [
+  { task: "A", anteriors: [] },
+  { task: "B", anteriors: [] },
+  { task: "C", anteriors: ["A"] },
+  { task: "E", anteriors: ["A"] },
+  { task: "D", anteriors: ["A", "B"] },
+  { task: "F", anteriors: ["C"] },
+  { task: "H", anteriors: ["E"] },
+  { task: "G", anteriors: ["D", "F"] },
+  { task: "I", anteriors: ["G"] },
+  { task: "J", anteriors: ["H", "I"] },
+];
+
+function getStartingTasks(precedenceConditions: PrecedenceCondition[]) {
+  return precedenceConditions.filter((condition) => condition.anteriors.length == 0).map((condition) => condition.task);
+}
+
+function getTasksSubsequents(precedenceConditions: PrecedenceCondition[]): TaskSubsequents[] {
+  const tasks = precedenceConditions.map((precedenceCondition) => precedenceCondition.task);
+  const tasksSubsequents: { [task: string]: string[] } = {};
+  for (const task of tasks) {
+    tasksSubsequents[task] = [];
+    for (const precedenceCondition of precedenceConditions) {
+      const anteriorsLength = precedenceCondition.anteriors.length;
+      if (precedenceCondition.anteriors[anteriorsLength - 1] == task) {
+        tasksSubsequents[task].push(precedenceCondition.task);
+      }
+    }
+  }
+  return Object.entries(tasksSubsequents).map(([key, value]) => ({ task: key, subsequents: value }));
+}
+
+function getTasksLevel(tasks: Task[]) {
+  let lastLevelNumber = 0;
+  const levels: Array<{ tasks: string[]; level: number }> = [];
+  levels.push({ tasks: getStartingTasks(precedenceConditions), level: lastLevelNumber });
+  for (const task of tasks) {
+    if (task.subsequents.length) {
+      lastLevelNumber = lastLevelNumber + 1;
+      levels.push({ tasks: task.subsequents, level: lastLevelNumber });
+    }
+  }
+  return levels;
 }
 
 function Pert() {
+  const tasks: Task[] = getTasksSubsequents(precedenceConditions).map((taskSubsequents, index) => ({
+    ...taskSubsequents,
+    ...precedenceConditions[index],
+  }));
 
-    var processedTasks = processTasks(tasks);
+  console.log(tasks);
 
-    return (
-        <TableContainer width="90%" borderRadius="xl" borderWidth='1px' borderColor="gray.500" padding="1rem" >
-            <Table variant='simple'>
-                <Thead>
-                    <Tr>
-                        <Th>Immediately anterior tasks</Th>
-                        <Th>Tasks to be done</Th>
-                        <Th>Immediately subsequent tasks</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {processedTasks.current.map((task, index) => (
-                        <Tr key={index}>
-                            <Td>{tasks.anterior[index].length > 0 ? tasks.anterior[index]?.join(",") : "-"}</Td>
-                            <Td>{task}</Td>
-                            <Td>{tasks.subsequent[index].length > 0 ? tasks.subsequent[index]?.join(",") : "-"}</Td>
-                        </Tr>
-                    ))}
-                </Tbody>
-            </Table>
-        </TableContainer>
-    )
+  console.log(getTasksLevel(tasks));
+
+  return (
+    <TableContainer width="90%" borderRadius="xl" borderWidth="1px" borderColor="gray.500" padding="1rem">
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Immediately anterior tasks</Th>
+            <Th>Tasks to be done</Th>
+            <Th>Immediately subsequent tasks</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tasks.map((task, index) => (
+            <Tr key={index}>
+              <Td>{task.anteriors.length > 0 ? task.anteriors.join(", ") : "-"}</Td>
+              <Td>{task.task}</Td>
+              <Td>{task.subsequents.length > 0 ? task.subsequents.join(", ") : "-"}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default Pert;
-
-function processTasks(tasks: Tasks) {
-
-    for (var i = 0; i < tasks.current.length; i++) {
-        var currentTask = tasks.current[i];
-        var subsequentTasks: string[] = [];
-        tasks.anterior.forEach((anteriorTasks, index) => {
-            if (
-                (anteriorTasks.length == 1 && anteriorTasks[0] == currentTask)
-                ||
-                (anteriorTasks.length > 1 && anteriorTasks[anteriorTasks.length - 1] == currentTask)
-            ) {
-                subsequentTasks.push(tasks.current[index]);
-            }
-        });
-        tasks.subsequent[i] = subsequentTasks;
-    }
-    return tasks;
-}
