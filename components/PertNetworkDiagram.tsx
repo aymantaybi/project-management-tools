edab07a4696c49fd072f3861bf2fff671c87fcc7
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2022-11-18 00:41:59 +0100

    refactor: wrap Pert class

--- components/PertNetworkDiagram.tsx
+++ components/PertNetworkDiagram.tsx
@@ -1,21 +1,5 @@
 import ReactFlow, { Position, Node, Edge, useNodesState, useEdgesState } from "react-flow-renderer";
-
-interface PrecedenceCondition {
-  task: string;
-  anteriors: string[];
-}
-
-interface TaskSubsequents {
-  task: string;
-  subsequents: string[];
-}
-
-interface Task extends PrecedenceCondition, TaskSubsequents {}
-
-interface ConvergentesTasks {
-  tasks: string[];
-  end: string;
-}
+import { Pert, PrecedenceCondition } from "../lib/pert-network";
 
 const precedenceConditions: PrecedenceCondition[] = [
   { task: "A", anteriors: [] },
@@ -30,192 +14,41 @@ const precedenceConditions: PrecedenceCondition[] = [
   { task: "J", anteriors: ["H", "I"] },
 ];
 
-function getBeginningTasks(precedenceConditions: PrecedenceCondition[]) {
-  return precedenceConditions.filter((condition) => condition.anteriors.length == 0).map((condition) => condition.task);
-}
-
-function getCompletingTasks(precedenceConditions: PrecedenceCondition[]) {
-  const tasks: string[] = [];
-  const anteriors: string[] = [];
-  for (const precedenceCondition of precedenceConditions) {
-    tasks.push(precedenceCondition.task);
-    anteriors.push(...precedenceCondition.anteriors);
-  }
-  return tasks.filter((task) => !anteriors.includes(task));
-}
-
-function getConvergentTasks(tasks: Task[]) {
-  const convergentTasks: ConvergentesTasks[] = [];
-  for (const task of tasks) {
-    if (task.subsequents.length == 0) {
-      const currentTaskWithNoSubsequents = task.task;
-      const taskWithCurrentTaskAsAnterior = tasks.find(
-        (task) => task.anteriors.length >= 2 && task.anteriors.includes(currentTaskWithNoSubsequents)
-      );
-      if (taskWithCurrentTaskAsAnterior) {
-        const { anteriors } = taskWithCurrentTaskAsAnterior;
-        for (const anterior of anteriors) {
-          if (anterior == currentTaskWithNoSubsequents) continue;
-          convergentTasks.push({
-            tasks: [currentTaskWithNoSubsequents, anterior],
-            end: taskWithCurrentTaskAsAnterior.task,
-          });
-        }
-      }
-    }
-  }
-  return convergentTasks;
-}
-
-function getTasksSubsequents(precedenceConditions: PrecedenceCondition[]): TaskSubsequents[] {
-  const tasks = precedenceConditions.map((precedenceCondition) => precedenceCondition.task);
-  const tasksSubsequents: { [task: string]: string[] } = {};
-  for (const task of tasks) {
-    tasksSubsequents[task] = [];
-    for (const precedenceCondition of precedenceConditions) {
-      const anteriorsLength = precedenceCondition.anteriors.length;
-      if (precedenceCondition.anteriors[anteriorsLength - 1] == task) {
-        tasksSubsequents[task].push(precedenceCondition.task);
-      }
-    }
-  }
-
-  console.log(tasksSubsequents);
-
-  return Object.entries(tasksSubsequents).map(([key, value]) => ({ task: key, subsequents: value }));
-}
-
-function getTasksLevel(tasks: Task[]) {
-  let lastLevelNumber = 0;
-  const levels: Array<{ tasks: string[]; level: number }> = [];
-  levels.push({ tasks: getBeginningTasks(precedenceConditions), level: lastLevelNumber });
-  for (const task of tasks) {
-    if (task.subsequents.length) {
-      lastLevelNumber = lastLevelNumber + 1;
-      levels.push({ tasks: task.subsequents, level: lastLevelNumber });
-    }
-  }
-  return levels;
-}
-
-function getTasksStep(task: string, levels: Array<{ tasks: string[]; level: number }>) {
-  const taskLevel = levels.find((level) => level.tasks.includes(task));
-  return taskLevel ? taskLevel.level + 1 : null;
-}
-
-const tasks: Task[] = getTasksSubsequents(precedenceConditions).map((taskSubsequents, index) => ({
-  ...taskSubsequents,
-  ...precedenceConditions[index],
-}));
-
-function getTaskSubsequents(task: string, tasks: Task[]) {
-  const currentTask = tasks.find((item) => item.task == task);
-  return currentTask?.subsequents ?? [];
-}
-
-class Network {
-  nodeStyle = {
-    width: 50,
-    height: 50,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "50%",
-  };
-  nodes: Node[];
-  edges: Edge[] = [];
-
-  defaultEdgesLength = { x: 180, y: 120 };
-
-  constructor() {
-    this.nodes = [
-      {
-        id: "1",
-        data: { label: "1" },
-        position: { x: 10, y: 250 },
-        targetPosition: Position.Left,
-        sourcePosition: Position.Right,
-        style: this.nodeStyle,
-      },
-    ];
-  }
-
-  addNode(node: Node) {
-    this.nodes.push(node);
-  }
-
-  addEdge(edge: Edge) {
-    this.edges.push(edge);
-  }
-}
-
-function taskConvergence(task: string, convergentTasks: ConvergentesTasks[]) {
-  return convergentTasks.find((item) => item.tasks.includes(task));
-}
+const nodeStyle = {
+  width: 50,
+  height: 50,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "50%",
+};
 
 export default function PertNetworkDiagram() {
-  const beginningTasks = getBeginningTasks(precedenceConditions);
-  const completingTasks = getCompletingTasks(precedenceConditions);
-  const convergentTasks = getConvergentTasks(tasks);
-  const levels = getTasksLevel(tasks);
+  const pert = new Pert(precedenceConditions);
 
-  const network = new Network();
+  console.log(pert.tasks());
+  console.log(pert.network());
 
-  for (const level of levels.concat({ level: levels[levels.length - 1].level + 1, tasks: [] })) {
-    if (level.level > 0) {
-      const { nodes } = network;
-      const lastNode = nodes[nodes.length - 1];
-      network.addNode({
-        id: String(level.level + 1),
-        data: { label: String(level.level + 1) },
-        position: { x: lastNode.position.x + 100, y: 250 },
-        targetPosition: Position.Left,
-        sourcePosition: Position.Right,
-        style: network.nodeStyle,
-      });
-    }
-  }
+  const { tasks, steps } = pert.network();
 
-  for (const currentTask of tasks.map((task) => task.task)) {
-    const currentTaskStep = getTasksStep(currentTask, levels);
-    const taskSubsequents = getTaskSubsequents(currentTask, tasks);
-    if (taskSubsequents.length) {
-      for (const taskSubsequent of taskSubsequents) {
-        const taskSubsequentStep = getTasksStep(taskSubsequent, levels);
-        network.addEdge({
-          id: `${currentTask}-${taskSubsequentStep}`,
-          source: String(currentTaskStep),
-          target: String(taskSubsequentStep),
-          type: "straight",
-          label: currentTask,
-        });
-      }
-    } else {
-      const currentTaskConvergence = taskConvergence(currentTask, convergentTasks);
-      if (currentTaskConvergence) {
-        const currentTaskConvergenceEndStep = getTasksStep(currentTaskConvergence.end, levels);
-        network.addEdge({
-          id: `${currentTask}-${currentTaskConvergenceEndStep}`,
-          source: String(currentTaskStep),
-          target: String(currentTaskConvergenceEndStep),
-          type: "straight",
-          label: currentTask,
-        });
-      } else if (completingTasks.includes(currentTask)) {
-        const finalStep = currentTaskStep ? currentTaskStep + 1 : null;
-        network.addEdge({
-          id: `${currentTask}-${finalStep}`,
-          source: String(currentTaskStep),
-          target: String(finalStep),
-          type: "straight",
-          label: currentTask,
-        });
-      }
-    }
-  }
-
-  const [nodes, setNodes, onNodesChange] = useNodesState(network.nodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(network.edges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(
+    steps.map((step) => ({
+      ...step,
+      data: { label: step.id },
+      position: { x: 10 + Number(step.id) * 100, y: 250 },
+      targetPosition: Position.Left,
+      sourcePosition: Position.Right,
+      style: nodeStyle,
+    }))
+  );
+  
+  const [edges, setEdges, onEdgesChange] = useEdgesState(
+    tasks.map((task) => ({
+      ...task,
+      type: "straight",
+      label: task.id,
+    }))
+  );
 
   return (
     <div
---
+++ lib/pert-network.ts
@@ -0,0 +1,179 @@
+export interface PrecedenceCondition {
+  task: string;
+  anteriors: string[];
+}
+
+interface TaskSubsequents {
+  task: string;
+  subsequents: string[];
+}
+
+interface Task extends PrecedenceCondition, TaskSubsequents {}
+
+interface ConvergentesTasks {
+  tasks: string[];
+  end: string;
+}
+
+interface TasksLevel {
+  tasks: string[];
+  level: number;
+}
+
+interface Network {
+  steps: Array<{ id: string }>;
+  tasks: Array<{ id: string; source: string; target: string }>;
+}
+
+export class Pert {
+  precedenceConditions: PrecedenceCondition[];
+
+  constructor(precedenceConditions: PrecedenceCondition[]) {
+    this.precedenceConditions = precedenceConditions;
+  }
+
+  beginningTasks() {
+    return this.precedenceConditions
+      .filter((condition) => condition.anteriors.length == 0)
+      .map((condition) => condition.task);
+  }
+
+  completingTasks() {
+    const tasks: string[] = [];
+    const anteriors: string[] = [];
+    for (const precedenceCondition of this.precedenceConditions) {
+      tasks.push(precedenceCondition.task);
+      anteriors.push(...precedenceCondition.anteriors);
+    }
+    return tasks.filter((task) => !anteriors.includes(task));
+  }
+
+  convergingTasks(tasks: Task[]) {
+    const convergentTasks: ConvergentesTasks[] = [];
+    for (const task of tasks) {
+      if (task.subsequents.length == 0) {
+        const currentTaskWithNoSubsequents = task.task;
+        const taskWithCurrentTaskAsAnterior = tasks.find(
+          (task) => task.anteriors.length >= 2 && task.anteriors.includes(currentTaskWithNoSubsequents)
+        );
+        if (taskWithCurrentTaskAsAnterior) {
+          const { anteriors } = taskWithCurrentTaskAsAnterior;
+          for (const anterior of anteriors) {
+            if (anterior == currentTaskWithNoSubsequents) continue;
+            convergentTasks.push({
+              tasks: [currentTaskWithNoSubsequents, anterior],
+              end: taskWithCurrentTaskAsAnterior.task,
+            });
+          }
+        }
+      }
+    }
+    return convergentTasks;
+  }
+
+  tasksSubsequents(precedenceConditions: PrecedenceCondition[]): TaskSubsequents[] {
+    const tasks = precedenceConditions.map((precedenceCondition) => precedenceCondition.task);
+    const tasksSubsequents: { [task: string]: string[] } = {};
+    for (const task of tasks) {
+      tasksSubsequents[task] = [];
+      for (const precedenceCondition of precedenceConditions) {
+        const anteriorsLength = precedenceCondition.anteriors.length;
+        if (precedenceCondition.anteriors[anteriorsLength - 1] == task) {
+          tasksSubsequents[task].push(precedenceCondition.task);
+        }
+      }
+    }
+    return Object.entries(tasksSubsequents).map(([key, value]) => ({ task: key, subsequents: value }));
+  }
+
+  tasksLevels(tasks: Task[]) {
+    let lastLevelNumber = 0;
+    const levels: Array<{ tasks: string[]; level: number }> = [];
+    levels.push({ tasks: this.beginningTasks(), level: lastLevelNumber });
+    for (const task of tasks) {
+      if (task.subsequents.length) {
+        lastLevelNumber = lastLevelNumber + 1;
+        levels.push({ tasks: task.subsequents, level: lastLevelNumber });
+      }
+    }
+    return levels;
+  }
+
+  taskStep(task: string, levels: TasksLevel[]) {
+    const taskLevel = levels.find((level) => level.tasks.includes(task));
+    return taskLevel ? taskLevel.level + 1 : null;
+  }
+
+  taskSubsequents(task: string, tasks: Task[]) {
+    const currentTask = tasks.find((item) => item.task == task);
+    return currentTask?.subsequents ?? [];
+  }
+
+  taskConvergence(task: string, convergentTasks: ConvergentesTasks[]) {
+    return convergentTasks.find((item) => item.tasks.includes(task));
+  }
+
+  steps(levels: TasksLevel[]) {
+    const steps = [];
+    for (const level of levels.concat({ level: levels[levels.length - 1].level + 1, tasks: [] })) {
+      steps.push({
+        id: String(level.level + 1),
+      });
+    }
+    return steps;
+  }
+
+  tasks(): Task[] {
+    return this.tasksSubsequents(this.precedenceConditions).map((taskSubsequents, index) => ({
+      ...taskSubsequents,
+      ...this.precedenceConditions[index],
+    }));
+  }
+
+  network(): Network {
+    const network: Network = {
+      steps: [],
+      tasks: [],
+    };
+
+    const tasks = this.tasks();
+    const beginningTasks = this.beginningTasks();
+    const completingTasks = this.completingTasks();
+    const convergentTasks = this.convergingTasks(tasks);
+    const levels = this.tasksLevels(tasks);
+    network.steps = this.steps(levels);
+
+    for (const currentTask of tasks.map((task) => task.task)) {
+      const currentTaskStep = this.taskStep(currentTask, levels);
+      const taskSubsequents = this.taskSubsequents(currentTask, tasks);
+      if (taskSubsequents.length) {
+        for (const taskSubsequent of taskSubsequents) {
+          const taskSubsequentStep = this.taskStep(taskSubsequent, levels);
+          network.tasks.push({
+            id: currentTask,
+            source: String(currentTaskStep),
+            target: String(taskSubsequentStep),
+          });
+        }
+      } else {
+        const currentTaskConvergence = this.taskConvergence(currentTask, convergentTasks);
+        if (currentTaskConvergence) {
+          const currentTaskConvergenceEndStep = this.taskStep(currentTaskConvergence.end, levels);
+          network.tasks.push({
+            id: currentTask,
+            source: String(currentTaskStep),
+            target: String(currentTaskConvergenceEndStep),
+          });
+        } else if (completingTasks.includes(currentTask)) {
+          const finalStep = currentTaskStep ? currentTaskStep + 1 : null;
+          network.tasks.push({
+            id: currentTask,
+            source: String(currentTaskStep),
+            target: String(finalStep),
+          });
+        }
+      }
+    }
+    return network;
+  }
+}
